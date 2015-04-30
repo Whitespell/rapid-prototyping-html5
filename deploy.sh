678ffcac17c2606a93a0943771eb8d8a91835686
Pim De Witte <pim@whitespell.com>
2015-04-30 02:33:34 -0400

    testlol

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-scp -r public/* $USER@23.236.50.248:/usr/share/nginx/www/peak/public/
-scp -r local/* $USER@23.236.50.248:/usr/share/nginx/www/peak/local/
+#scp -r public/* $USER@23.236.50.248:/usr/share/nginx/www/peak/public/
+#scp -r local/* $USER@23.236.50.248:/usr/share/nginx/www/peak/local/
+git add --all
+git commit -m $1
+git push origin master
+ssh public.whitespell.com
+cd /etc/share/nginx/www/peak
+git pull origin master
