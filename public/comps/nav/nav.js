6cc6ab9f9c109dfd06132478f292e6c3f5c0e58f
Pim De Witte <pim@whitespell.com>
2015-05-01 02:25:59 -0400

    full-test

--- public/comps/nav/nav.js
+++ public/comps/nav/nav.js
@@ -2,7 +2,14 @@ var gestures = {
 
 
     unique: { // ids
-
+        "notificationButton": {
+            release: function(el) {
+                wsUI.layout.updatePage({
+                    components: ["home"],
+                    updates: ["home->notifications"]
+                }, true);
+            }
+        }
     },
 
     collective: { //classes
