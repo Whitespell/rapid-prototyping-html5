b080efac6c9c6f812ab5938909af4cb1e19ad152
Pim De Witte <pim@whitespell.com>
2015-05-02 10:55:00 -0400

    test

--- public/comps/home/search/search.js
+++ public/comps/home/search/search.js
@@ -1,1 +1,58 @@
-testsearch
+var followToggles;
+
+var toggleFollow = function (id) {
+    console.log(id);
+    var toggled = followToggles[id];
+    if (toggled !== true) {
+        followToggles[id] = true;
+        DOM.transform(document.getElementById("followButton-"+id), "background", "#2e88bf");
+        DOM.transform(document.getElementById("followButton-"+id), "color", "#FFFFFF");
+        document.getElementById("followButton-"+id).innerHTML = '<i class="fa fa-user-times"></i> Unfollow';
+    } else {
+        followToggles[id] = false;
+        document.getElementById("followButton-"+id).innerHTML = '<i class="fa fa-flash"></i> Follow';
+        DOM.transform(document.getElementById("followButton-"+id), "color", "#212121");
+        DOM.transform(document.getElementById("followButton-"+id), "background", "none");
+    }
+}
+
+var gestures = {
+
+    unique: { // ids
+
+    },
+
+    collective: { //classes
+
+        "followButton": {
+            touch: function (el) {
+                DOM.transform(el, "opacity", "0.3");
+            },
+            release: function (el) {
+                toggleFollow(el.getAttribute("data-id"));
+            },
+            after: function(el) {
+                DOM.transform(el, "opacity", "1");
+            },
+            cancel: function(el) {
+                DOM.transform(el, "opacity", "1");
+            }
+        },
+        "moreButton": {
+            touch: function (el) {
+                DOM.transform(el, "opacity", "0.3");
+            },
+            release: function (el) {
+                alert("More info");
+            },
+            after: function(el) {
+                DOM.transform(el, "opacity", "1");
+            },
+            cancel: function(el) {
+                DOM.transform(el, "opacity", "1");
+            }
+        }
+    }
+}
+
+wsUI.ui.addGestures("{{componentName}}", gestures);
